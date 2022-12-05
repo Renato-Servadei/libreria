87248c0993be3dc95e907663beebe581a932e5d6
Rober <boblawyer1@gmail.com>
2022-12-05 20:38:53 -0300

    implementado el logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user
 
 from .models.ModeloLibro import ModeloLibro
 from .models.ModeloUsuario import ModeloUsuario
 from .models.entities.Usuario import Usuario
-
 
 
 app = Flask(__name__)
@@ -15,21 +14,22 @@ csrf = CSRFProtect()
 db = MySQL(app)
 login_manager_app = LoginManager(app)
 
+
 @login_manager_app.user_loader
 def load_user(id):
     return ModeloUsuario.obtener_por_id(db, id)
 
+
 @app.route('/')
 def index():
-        return render_template('index.html')
+    return render_template('index.html')
 
-
-        
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        usuario = Usuario(None, request.form['usuario'], request.form['password'], None)
+        usuario = Usuario(
+            None, request.form['usuario'], request.form['password'], None)
         usuario_logeado = ModeloUsuario.login(db, usuario)
         if usuario_logeado != None:
             login_user(usuario_logeado)
@@ -38,6 +38,12 @@ def login():
             return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/libros')
@@ -51,6 +57,7 @@ def listar_libros():
     except Exception as ex:
         print(ex)
         return "problema"
+
 
 def pagina_no_encontrada(error):
     return render_template('errores/404.html'), 404
